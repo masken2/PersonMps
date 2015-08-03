e62aaa2512767639a1cb4cad4ea4d60653aa2eef
mask2 <zhuangmask123@163.com>
2015-08-03 22:03:21 +0800

    1.建立了启动页面

---
+++ PersonMp3/src/com/example/personmp3/StartActivity.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.example.personmp3;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * @author zhuangAH
+ *
+ */
+public class StartActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+}
